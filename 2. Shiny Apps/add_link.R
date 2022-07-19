2d474ba4383e1638178e55dc3fef1f3f81b5b9fd
Farooq Khan <105917259+khanfs@users.noreply.github.com>
2022-07-19 11:13:11 +0100

    Delete 2. Shiny Apps directory

--- 2. Shiny Apps/add_link.R
+++
@@ -1,4 +0,0 @@
-Add_link <- as.data.table(matrix(c(input$from,input$to  #"ESTU_ESTRATO","PUNTAJE"
-           ), ncol = 2, byrow = T))[,.(from = V1, to = V2)] # 'From' and 'to' are the names of the columns
-
-White_list <- rbind(White_list,Add_link)
--- 2. Shiny Apps/run_and_deploy_app.R
+++
@@ -1,7 +0,0 @@
-## Run app
-library(shiny)
-runApp()
-
-## Deploy app to cloud
-library(rsconnect)
-deployApp()
